a3d16de0d8873b087cf9bb956917a3d7cf5eea1e
ingramwd <Ingramwd01@gmail.com>
2022-01-23 22:35:31 -0500

    askBCS

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const { writeFileSync } = require('fs');
+const { note } = require('../db/db.json');
+fs = require('fs');
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, ('../db/db.json')));
+    res.readFile('../db/db.json'), (err, data) => {
+        if (err) throw err;
+        notes = JSON.parse(data);
+        res.send(notes);
+    };
+});
+
+router.post('/notes', (req, res) => {
+    console.log(req.body)
+    writeFileSync(req.body)
+        .then(note => {
+            res.json(note)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
 });
 
 module.exports = router;
