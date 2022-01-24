43ddb02817f78fc72f95e6795baa23444a539adc
ingramwd <Ingramwd01@gmail.com>
2022-01-23 21:03:29 -0500

    added and tested html routes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API sever now on port ${PORT}`);
+});
